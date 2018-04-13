b3c5032b0568b6efd90d4f6be8f3f51659d391cf
davidlav <lavergne.4@osu.edu>
2018-04-13 13:29:06 -0500

    vehicles.json loaded

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 export class App {
   message = 'Hello World!';
+  data;
+
+  constructor() {
+    this.getData();
+  }
+
+  async getData() {
+    const resp = await fetch('data/vehicles.json');
+    this.data = await resp.json();
+    console.log(this.data);
+  }
+
 }
